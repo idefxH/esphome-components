d6bce06c0878ec699b3c390430e5381733cf89a6
idefxH <73938941+idefxH@users.noreply.github.com>
2023-11-25 15:26:16 +0100

    sdfsdf

--- components/personal_toolbox/__init__.py
+++ components/personal_toolbox/__init__.py
@@ -1,3 +1,17 @@
-import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import personal_toolbox
+import esphome.codegen as cg
+from esphome.components import sensor, binary_sensor
+from esphome.const import CONF_ID, CONF_MAC_ADDRESS
+
+# Configuration schema
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(PersonalToolbox),
+    cv.Required(CONF_MAC_ADDRESS): cv.mac_address,
+}).extend(cv.COMPONENT_SCHEMA)
+
+# Code generation
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    yield cg.register_component(var, config)
+    yield sensor.register_sensor(var, config)
+    yield binary_sensor.register_binary_sensor(var, config)
