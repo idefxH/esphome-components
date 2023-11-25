b6dd77fd958b23cfed53d4a4519dcf93772f9ec4
idefxH <73938941+idefxH@users.noreply.github.com>
2023-11-25 17:43:08 +0100

    sdfsdf

--- components/personal_toolbox/display_toolbox.h
+++ components/personal_toolbox/display_toolbox.h
@@ -12,9 +12,13 @@ struct x_y_coordinates{
 
 class screen_tile
 {
-    lilygo_t5_47::LilygoT547Display test;
+    lilygo_t5_47::LilygoT547Display provided_display;
     x_y_coordinates top_left;
     x_y_coordinates top_right;
     x_y_coordinates bottom_left;
     x_y_coordinates bottom_right;
+    void display_it(){
+        provided_display->update();
+    }
+    
 };
